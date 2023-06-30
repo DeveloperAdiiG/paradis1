16c195e1b2653747f6f682d56e2a0bed5d3e0871
DeveloperAdiiG <114342189+DeveloperAdiiG@users.noreply.github.com>
2023-06-30 22:25:22 +0530

    Update smtp.js

--- smtp.js
+++ smtp.js
@@ -1,6 +1,6 @@
 function sendEmail(){
     Email.send({
-        SecureToken : "2d0f5046-16dd-4d3e-8b36-2fdf650bd0a4"
+        SecureToken : "2d0f5046-16dd-4d3e-8b36-2fdf650bd0a4" ,
         To : 'adii.dg.2021@gmail.com',
         From : "adityagurav54@gmail.com",
         Subject : "New Enquiry from : "+document.getElementById("mail").value,
