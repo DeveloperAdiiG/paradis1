2955d5c323b3f17bcb0b5a3c08774ef554bb200a
DeveloperAdiiG <114342189+DeveloperAdiiG@users.noreply.github.com>
2023-06-30 22:24:50 +0530

    Update smtp.js

--- smtp.js
+++ smtp.js
@@ -1,8 +1,6 @@
 function sendEmail(){
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "adityagurav54@gmail.com",
-        Password : "04BD95B5065355D3B03934DFB51A9EF28A43",
+        SecureToken : "2d0f5046-16dd-4d3e-8b36-2fdf650bd0a4"
         To : 'adii.dg.2021@gmail.com',
         From : "adityagurav54@gmail.com",
         Subject : "New Enquiry from : "+document.getElementById("mail").value,
